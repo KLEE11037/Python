c896bc7ee36cdae39353ebabc068d9cbe7411e65
Кира <zakiraza2009@yandex.ru>
2026-04-21 21:07:23 +0300

    Решение задач

---
+++ 4. Циклы/4.2 Цикл while/Задачи/ЕГЭ.py
@@ -0,0 +1,9 @@
+for x in range (2):
+    for y in range (2):
+        for z in range (2):
+            for w in range (2):
+                f = (x<=y and not z) or w
+                if f == 0 :
+                    break
+
+                print (x, y, z, w, f)
